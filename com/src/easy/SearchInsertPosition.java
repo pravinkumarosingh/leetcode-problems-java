3839004531d4e9a6b08e585b3f000a5ab1111633
pravinkumarosingh <pravinkumarosingh@gmail.com>
2024-02-22 22:14:53 +0530

    search insert position

---
+++ com/src/easy/SearchInsertPosition.java
@@ -0,0 +1,23 @@
+package com.src.easy;
+
+public class SearchInsertPosition {
+    public static void main(String[] args) {
+
+        int[] nums = {1,3,5,6};
+        int target = 4;
+
+        System.out.println(searchPosition(nums,target));
+    }
+
+    private static int searchPosition(int[] numbers, int target){
+
+        for(int i = 0 ; i < numbers.length ; i++){
+            if(numbers[i] == target){
+                return i;
+            } else if (target < numbers[i]){
+                return i;
+            }
+        }
+        return numbers.length;
+    }
+}
