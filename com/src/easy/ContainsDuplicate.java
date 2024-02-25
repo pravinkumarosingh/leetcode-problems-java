988c08349a943f20695318ae19db353b09267e0f
pravinkumarosingh <pravinkumarosingh@gmail.com>
2024-02-25 21:43:15 +0530

    contains duplicate 1

---
+++ com/src/easy/ContainsDuplicate.java
@@ -0,0 +1,26 @@
+package com.src.easy;
+
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class ContainsDuplicate {
+    public static void main(String[] args) {
+        int[] nums = {1,1,1,3,3,4,3,2,4,2};
+
+        System.out.println(containsDuplicate(nums));
+    }
+
+    private static boolean containsDuplicate(int[] numbers){
+        Map<Integer, Integer> map = new HashMap<>();
+
+        for ( int i = 0 ; i < numbers.length ; i++){
+            if (map.containsKey(numbers[i])){
+                return true;
+            } else {
+                map.put(numbers[i], 1);
+            }
+        }
+        return false;
+    }
+}
