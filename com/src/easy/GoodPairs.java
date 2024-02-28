6fe14137e669a5188e3e55197266bb7caee605ef
pravinkumarosingh <pravinkumarosingh@gmail.com>
2024-02-28 21:16:40 +0530

    good pairs in an array

---
+++ com/src/easy/GoodPairs.java
@@ -0,0 +1,20 @@
+package com.src.easy;
+
+public class GoodPairs {
+    public static void main(String[] args) {
+        int[] nums = {1,1,1,1};
+        System.out.println(goodPairs(nums));
+    }
+
+    private static int goodPairs(int[] nums){
+        int counter = 0;
+        for (int i = 0 ; i < nums.length ; i++){
+            for (int j = 0 ; j < nums.length ; j++){
+                if( i < j && nums[i] == nums[j]){
+                    ++counter;
+                }
+            }
+        }
+        return counter;
+    }
+}
