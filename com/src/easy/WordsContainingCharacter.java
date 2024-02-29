fc24f7dd777bdd1b313f04e2da38c697f424cece
pravinkumarosingh <pravinkumarosingh@gmail.com>
2024-02-29 19:53:22 +0530

    words containing character

---
+++ com/src/easy/WordsContainingCharacter.java
@@ -0,0 +1,22 @@
+package com.src.easy;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class WordsContainingCharacter {
+    public static void main(String[] args) {
+        String[] words = {"abc","bcd","aaaa","cbc"};
+        char x = 'a';
+        System.out.println(getIndices(words,x));
+    }
+
+    private static List<Integer> getIndices(String[] words, char x){
+        List<Integer> indices = new ArrayList<>();
+        for (int i = 0 ; i < words.length ; i++){
+            if(words[i].indexOf(x) >= 0){
+                indices.add(i);
+            }
+    }
+        return indices;
+    }
+}
